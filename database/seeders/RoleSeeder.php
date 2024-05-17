f9721e04a6f5de765d194b6c7b6cd24c0eabee43
Adán Amolitos <adanamolitos@gmail.com>
2024-05-16 18:24:46 -0600

    feat: update user roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -10,5 +10,7 @@ class RoleSeeder extends Seeder
     public function run()
     {
         Role::create(['name' => 'admin']);
+        Role::create(['name' => 'manager']);
+        Role::create(['name' => 'seller']);
     }
 }
