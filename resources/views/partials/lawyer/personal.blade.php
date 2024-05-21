01d249c44bc04f8e4f67d96d7900859c272c2727
Adán Amolitos <adanamolitos@gmail.com>
2024-05-20 22:34:49 -0600

    temp: hide favorite button

--- resources/views/partials/lawyer/personal.blade.php
+++ resources/views/partials/lawyer/personal.blade.php
@@ -1,8 +1,8 @@
 <div class="flex flex-col items-center gap-4 text-center">
     <div class="h-72 w-72 flex justify-center items-center"
         style="background-image: url({{ asset('images/profile.svg') }})">
-        <img src="{{ $lawyer->profile->photo ? '/storage/' . $lawyer->profile->photo : '/images/user.jpg' }}" alt="Abogado"
-            class="h-52 w-52 object-contain rounded-full select-none mr-2 mb-3">
+        <img src="{{ $lawyer->profile->photo ? '/storage/' . $lawyer->profile->photo : '/images/user.jpg' }}"
+            alt="Abogado" class="h-52 w-52 object-contain rounded-full select-none mr-2 mb-3">
     </div>
     <div>
         <h1 class="font-semibold text-2xl text-secondary dark:text-primary">
@@ -16,10 +16,10 @@
                 <i class="fa-solid fa-star mr-1"></i>
             @endfor
         </p>
-        <form method="POST" action="" class="mt-4">
+        {{-- <form method="POST" action="" class="mt-4">
             <button type="submit" class="btn border-primary text-primary rounded-3xl">
                 Favorito
             </button>
-        </form>
+        </form> --}}
     </div>
 </div>
