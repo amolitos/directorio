3d5a1809663ecc0eaef63d0a8038955cea9bdbe7
Adán Amolitos <adanamolitos@gmail.com>
2024-06-12 19:39:00 -0600

    style: update lawyer item for mobile in search

--- resources/js/components/Search/LawyerItem.jsx
+++ resources/js/components/Search/LawyerItem.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export function LawyerItem({ lawyer }) {
+  function capitalizeWords(str) {
+    return str
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+  }
+
   return (
     <a href={lawyer.url} className="card flex items-start gap-5">
       <img
@@ -8,16 +15,18 @@ export function LawyerItem({ lawyer }) {
         alt="Foto de perfil"
         className="w-16 md:w-20 h-16 md:h-20 rounded-full"
       />
-      <div className="relative grow">
-        {lawyer.verified_at && (
-          <div className="absolute top-1 right-1 bg-teal-600 text-sm text-white rounded py-1 px-2">
-            <i className="fa-solid fa-check mr-2" />
-            VERIFICADO
-          </div>
-        )}
-        <h3 className="font-semibold md:text-xl truncate leading-none">
-          {lawyer.name}
-        </h3>
+      <div className="w-full overflow-x-hidden">
+        <div className="flex items-center">
+          <h3 className="font-semibold md:text-xl truncate">
+            {capitalizeWords(lawyer.name)}
+          </h3>
+          {lawyer.verified_at && (
+            <div className="w-fit bg-teal-600 text-sm text-white rounded-full md:rounded py-1 px-2 ml-auto">
+              <i className="fa-solid fa-check md:mr-2" />
+              <span className="hidden md:inline-block">VERIFICADO</span>
+            </div>
+          )}
+        </div>
         <div className="badge badge-primary mt-2">{lawyer.degree}</div>
         <p className="text-sm md:text-base text-stone-600 dark:text-gray-400 mt-2">
           {`${lawyer.state}, ${lawyer.city}`}
