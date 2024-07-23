913f8b93fecb45a13cb7cb0f72fc2de043a3f163
Adán Amolitos <adanamolitos@gmail.com>
2024-07-23 12:01:38 -0600

    config: lang es

---
+++ lang/es/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'success_store' => 'Se agregó :text exitosamente.',
+    'success_update' => 'El registro se actualizó exitosamente.',
+    'success_update_custom' => ':text se actualizó exitosamente.',
+    'success_delete' => 'El registro se eliminó exitosamente.',
+    'success_upload' => 'El documento (:file), se subió exitosamente.',
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+];
